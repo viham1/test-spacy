c51aa0e0e14405926f38ed0348a84bb8666acbcb
viham1 <65411630+viham1@users.noreply.github.com>
2023-01-31 17:02:27 +0100

    Update

--- task.py
+++ task.py
@@ -1,3 +1,3 @@
 import spacy
 
-spacy.cli.train.train("config.cfg", output_path="../../outputs/model",overrides={"paths.train": "train.spacy", "paths.dev": "test.spacy"},use_gpu=0)
+spacy.cli.train.train("config.cfg", output_path="/model",overrides={"paths.train": "train.spacy", "paths.dev": "test.spacy"},use_gpu=0)
